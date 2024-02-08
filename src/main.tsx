fa31d3df311266e3eb63c65ca2db431970ce0134
Richard Bann <richard.bann@galaktika.solutions>
2024-02-08 11:46:18 +0000

    adding some style to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import clsx from "clsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div className="overflow-hidden">
+      <div className="w-screen">
+        <div className={clsx("mx-auto", "p-3", "max-w-screen-lg", "text-slate-700")}>
+          <App />
+        </div>
+      </div>
+    </div>
+  </React.StrictMode>
+);
